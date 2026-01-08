a95ee4d4eef9217149882da95ef8d4c5844c255d
jesh-rc <jesh7422@gmail.com>
2026-01-08 00:20:14 -0500

    Added Nav bar component and created pages for services, resume, work, and contact
    - Updated header with logo, desktop nav, hire me button, and mobile nav
    - Created Nav component with pages
    - Created folders for each page in nav bar, in app folder

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,31 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+// Components
+import Nav from "./Nav";
+
 const Header = () => {
   return (
-    <header>header</header>
+    <header className="py-8 xl:py-12 text-white">
+        <div className="container mx-auto flex justify-between items-center">
+            {/* logo */}
+            <Link href="/">
+                <h1 className="text-4xl font-semibold">
+                    jesh<span className="text-accent">.</span>rc
+                </h1>
+            </Link>
+            {/* desktop nav & hire me button */}
+            <div className="hidden xl:flex items-center gap-8">
+                <Nav />
+                <Link href="/contact">
+                    <Button>Hire Me</Button>
+                </Link>
+            </div>
+
+            {/* mobile nav */}
+            <div className="xl:hidden">mobile nav</div>
+        </div>
+    </header>
   );
 };
 
