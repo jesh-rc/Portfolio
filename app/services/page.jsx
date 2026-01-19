180ce76e2ab926764d06043044e5316f939ffd31
jesh-rc <jesh7422@gmail.com>
2026-01-18 22:29:07 -0500

    Started working on Services page
    - Added 4 services and started on styling

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,6 +1,72 @@
+"use client";
+
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+
+
+const services = [
+    {
+        num: '01',
+        title: 'Web Development',
+        description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        href: ""
+    },
+    {
+        num: '02',
+        title: 'UI/UX Design',
+        description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        href: ""
+    },
+    {
+        num: '03',
+        title: 'Logo Design',
+        description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        href: ""
+    },
+    {
+        num: '04',
+        title: 'SEO',
+        description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        href: ""
+    },
+];
+
+import { motion } from "framer-motion";
+
 const Services = () => {
     return (
-        <div>services page</div>
+        <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+            <div className="container mx-auto">
+                <motion.div
+                    initial={{opacity: 0}}
+                    animate={{
+                        opacity: 1,
+                        transition: {delay: 2.4, duration: 0.4, ease: "easeIn"},
+                    }}
+                    className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+                >
+                    {services.map((service, index) => {
+                        return (
+                            <div key={index} className="flex-1 flex flex-col justify-center gap-6 group">
+                                {/* top */}
+                                <div className="w-full flex justify-between items-center">
+                                    <div className="text-5xl font-extrabold text-outline text-transparent group-hover:[&]:text-outline-hover">{service.num}</div>
+                                    <Link href={service.href}>
+                                        <BsArrowDownRight />
+                                    </Link>
+                                </div>
+                                {/* heading */}
+                                <h2>{service.title}</h2>
+                                {/* description */}
+                                <p>{service.description}</p>
+                                {/* border */}
+                                <div className="border-b border-white/20 w-full"></div>
+                            </div>
+                        );
+                    })}
+                </motion.div>
+            </div>
+        </section>
     );
 };
 
