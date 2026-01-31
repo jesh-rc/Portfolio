c52de09b24d7e97a0b0d5632fe0fc99e15b5400e
jesh-rc <jesh7422@gmail.com>
2026-01-31 18:38:25 -0500

    Removed Services page from nav bar and social buttons open in new tab

--- components/ui/Social.jsx
+++ components/ui/Social.jsx
@@ -1,24 +1,26 @@
-import Link from "next/link";
-
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const socials = [
-    {icon: <FaGithub />, path: "https://github.com/jesh-rc"},
-    {icon: <FaLinkedin />, path: "http://linkedin.com/in/jeshurun-constantine"},
-]
+  { icon: <FaGithub />, path: "https://github.com/jesh-rc" },
+  { icon: <FaLinkedin />, path: "https://www.linkedin.com/in/jeshurun-constantine" },
+];
 
-const Social = ({containerStyles, iconStyles}) => {
-    return (
-        <div className={containerStyles}>
-            {socials.map((item, index) => {
-                return (
-                    <Link key={index} href={item.path} className={iconStyles}>
-                        {item.icon}
-                    </Link>
-                );
-                })}
-        </div>
-    )
-}
+const Social = ({ containerStyles, iconStyles }) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => (
+        <a
+          key={index}
+          href={item.path}
+          className={iconStyles}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {item.icon}
+        </a>
+      ))}
+    </div>
+  );
+};
 
 export default Social;
